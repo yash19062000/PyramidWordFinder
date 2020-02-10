fa9066e3bad1776c4beb7fe416422ab9cdbece4f
yash19062000 <56010134+yash19062000@users.noreply.github.com>
2020-02-09 23:14:05 -0600

    Add files via upload

---
+++ Models/Pyramid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace PyramidWord.Models
+{
+    [DataContract]
+    public class Pyramid
+    {
+        [DataMember(Name = "result")]
+        public string Result { get; set; }
+    }
+}
